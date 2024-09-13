7a3178e04c92e40a2f71748b09e416aaaf8c6140
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-09-12 23:15:03 -0300

    finalizando front-end

---
+++ readme.md
@@ -0,0 +1,9 @@
+npm i @biomejs/biome -D
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+npm i tailwind-merge
+npm i tailwind-variants
+npm i @radix-ui/react-radio-group
+npm i @tanstack/react-query
+npm i sonner
+npm install @hookform/resolvers
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
 import './index.css'
 
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'sonner'
+
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-  
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <App />
+
+      <Toaster position="bottom-left" richColors />
+    </QueryClientProvider>
+  </StrictMode>
 )
